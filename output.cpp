48d77b9e8dd0fffa4f368f9d26809d2bdb6477fe
kikidwimaulana111 <solarsg52@gmail.com>
2026-09-03 09:44:58 +0700

    Create output.cpp

---
+++ output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string namaBarang = "Laptop";
+    long long harga = 7500000;
+    int jumlah = 2;
+    string stok = "Tersedia";
+
+    cout << "===== DATA BARANG =====" << endl;
+    cout << "Nama Barang : " << namaBarang << endl;
+    cout << "Harga       : Rp" << harga << endl;
+    cout << "Jumlah      : " << jumlah << endl;
+    cout << "Stok        : " << stok << endl;
+
+    return 0;
+}
