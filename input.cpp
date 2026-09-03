6f1487ec1103a29bd6831b50dbec9a4c54677511
kikidwimaulana111 <solarsg52@gmail.com>
2026-09-03 09:39:20 +0700

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama;
+    int umur;
+
+    cout << "kiki: ";
+    cin >> nama;
+    cout << "15: ";
+    cin >> umur;
+
+    cout << "\nNama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+
+    return 0;
+}
